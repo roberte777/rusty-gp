5ca81d16c56233788a9fe0e931be9e56b44c0bb2
roberte777 <robewilkes@yahoo.com>
2022-12-14 01:53:43 -0600

    giving up for a while

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,8 +1,21 @@
 use crate::Genotype;
 
+pub enum GenerationMethod {
+    RampedHalfAndHalf,
+    Full,
+}
+
 pub struct TreeGenotype {
     pub fitness: f64,
     pub tree: Tree,
+}
+impl TreeGenotype {
+    pub fn new(method: GenerationMethod, max_depth: u32) -> Self {
+        Self {
+            fitness: 0.0,
+            tree: Tree::new(),
+        }
+    }
 }
 impl Genotype for TreeGenotype {
     fn fitness(&self) -> f64 {
@@ -17,11 +30,46 @@ impl Genotype for TreeGenotype {
     }
 }
 pub struct Tree {}
+impl Tree {
+    fn new() -> Self {
+        Tree {}
+    }
+}
 impl Tree {}
+pub trait Node {
+    fn evaluate(&self) -> f64;
+}
+
+pub struct InternalNode {
+    pub op: fn(f64, f64) -> f64,
+    pub left: Option<Box<dyn Node>>,
+    pub right: Option<Box<dyn Node>>,
+}
+impl InternalNode {
+    fn new(op: fn(f64, f64) -> f64) -> Self {
+        InternalNode {
+            op,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+impl Node for InternalNode {
+    fn evaluate(&self) -> f64 {
+        (self.op)(
+            self.left.as_ref().unwrap().evaluate(),
+            self.right.as_ref().unwrap().evaluate(),
+        )
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_tree() {}
+    fn test_tree() {
+        let genotype = TreeGenotype::new(GenerationMethod::Full, 5);
+        // assert_eq!(tree.evaluate(), 0.0);
+    }
 }
